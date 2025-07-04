8af05d138a52ca4afa9f0a9fa3aa5032717b6f90
Rashmiranjan-Mishra <rashmiranjan143mishra@gmail.com>
2025-07-05 00:47:59 +0530

    This is my First commit

---
+++ src/com/git/project/Test.java
@@ -0,0 +1,17 @@
+package com.git.project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   String hungry = "Hello";
+   
+   if(hungry == "Yes")
+	   System.out.println("eat something");
+   else
+   {
+	   System.out.println("Do something");
+   }
+	}
+
+}
